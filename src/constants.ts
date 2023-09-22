1fe43746458688e46feb20e4b159c4738f56f184
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2023-09-22 18:46:01 +0200

    fix: product data selector & graphql ID

--- src/constants.ts
+++ src/constants.ts
@@ -9,13 +9,13 @@ export const GRAPHQL_PRODUCTS_DATA_SEL = 'script.re-data-el-hydrate';
 /** In the format such as: 'Page 1 of 428' */
 export const PAGES_COUNT_SEL = 'nav .KxHAYs._2kjxJ6.FxZV-M';
 
-export const GRAPHQL_PRODUCT_DATA_SEL = 'script[data-re-asset][type="application/json"].re-1-14';
+export const GRAPHQL_PRODUCT_DATA_SEL = 'script[data-re-asset][type="application/json"].re-1-15';
 
 // Check the up-to-date GraphQL IDs in case there's an error parsing the data
 
 export const SIMPLE_PRODUCT_GRAPHQL_ID = '16d2c8077be7112ec28bddc382e2c2461dd847a0e68e706fec85f2ecb4e3ab76';
 export const PRODUCT_IMAGES_GRAPHQL_ID = '3dd015bcefc841c545bd6075391dfdea659de84f8e9a9e91d79c677c5321423f';
-export const PRODUCT_ATTRIBUTES_GRAPHQL_ID = '47537ef9971a026be7342267f8b1b738ec5c9b1d47e5a13446084eaa663dd3e5';
+export const PRODUCT_ATTRIBUTES_GRAPHQL_ID = 'e0e4bf6505b88d3b4fa02bf9705b113c6aa2768ebc1d74a887df9b5cae6e3436';
 export const GENERAL_PRODUCT_INFO_GRAPHQL_ID = 'f4e67f9df66088acdc15daf9d655deafc7066c062e206fab792331a4c8f1ee7c';
 export const RATING_REVIEWS_GRAPHQL_ID = '8c2e797421e1f6aeefde251780bc2a56f6246effa19419b61876f9081564cfa1';
 export const PRICE_CURRENCY_GRAPHQL_ID = '40f98d3b222207f4128f4db8948dc15188471748c1a3a594741da3517b279fb8';
